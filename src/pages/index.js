360e28a4c4df608469e2923e8aab76e170b9bf7e
Wallison Martins <wallisonpinheiro1@gmail.com>
2021-05-31 16:22:49 -0300

    add correction on dir components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
-import { Header } from '../Components/Header';
-import { Main } from '../Components/Main';
+import { Header } from '../components/Header';
+import { Main } from '../components/Main';
 import { Footer } from '../components/Footer';
 
 export default function Home() {
